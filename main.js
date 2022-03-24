a177b4bf360103f579d9f63150eca5fdbe45c295
Susan Bontia <susanbontia@gmail.com>
2022-03-24 22:16:41 +1300

    added value to app

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Boots',
-            description: 'the socks is blue'
+            description: 'the socks is blue',
+            value: '150'
         }
     }
 })
