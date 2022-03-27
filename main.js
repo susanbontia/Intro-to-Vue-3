c1f85e3ab4f1e2b6c91bf81b260050b46b040349
Susan Bontia <susanbontia@gmail.com>
2022-03-27 21:17:05 +1300

    Added list rendering

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'www.google.com',
             inventory: 100,
-            onSale: false
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            ],
+            sizes: ['small', 'medium', 'large']
         }
     }
 })
